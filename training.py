9d1e0dff1324e617857d46117345008b3a22e5a7
kellen-sun <ksun5@ocdsb.ca>
2024-01-03 08:57:15 -0500

    .

--- training.py
+++ training.py
@@ -37,7 +37,7 @@ for i in range(len(test_img)):
     test_y.append([Value(1) if j==test_lab[i] else Value(-1) for j in range(10)])
 print("Data setup...")
 
-n = MLP(784, [10, 10])
+n = MLP(784, [20, 20, 10])
 print("MLP created...")
 # print("number of parameters", len(n.parameters()))
 
@@ -51,18 +51,21 @@ def findloss(predvals, train_y):
         err += sum([(yi - scorei)**2
                      for yi, scorei in zip(j, i)])
         # still a value type
+    #print(max(range(10), key=lambda k: predvals[0][k].data), max(range(10), key=lambda k: train_y[0][k].data))
     acc = [max(range(10), key=lambda k: i[k].data) == max(range(10), key=lambda k: j[k].data) for i,j in zip(predvals, train_y)]
-    return err * (1.0 / s), sum(acc)/s
+    return err, sum(acc)/s
 
 #training process:
-def train():
-    learningrate = 0.5
-    numofpasses = 5
-    batchsize = 8
-    counter = 0
-    for batch in range(100):
-        print("Training batch", batch+1, "of 100...")
-        for i in range(numofpasses):
+def train(bsize, alpha, num, bcount):
+    learningrate = alpha
+    numofpasses = num
+    batchsize = bsize
+    
+    for i in range(numofpasses):
+        counter = 0
+        print("Training", i+1, "of", numofpasses, "...")
+        for batch in range(bcount):
+            print("    Training batch", batch+1, "of", bcount, "...")
             # forward pass
             predvals = list(map(n, train_x[counter:counter+batchsize]))
             loss, acc = findloss(predvals, train_y[counter:counter+batchsize])
@@ -70,61 +73,56 @@ def train():
             for p in n.parameters():
                 p.grad = 0.0
             loss.backwards()
-            print("    Loss:", loss.data, "Acc:", acc)
-            print("    Training", i+1, "of", numofpasses, "...")
+            print("    Loss:", loss.data, "Acc:", round(100*acc,2), "%")
             # update
-            learningrate = 0.5 - 0.45 * (batch+i)/(100+numofpasses)
+            learningrate = alpha - alpha * i/numofpasses
             for p in n.parameters():
                 p.data -= learningrate * p.grad
             
-        counter+=batchsize
-        if batch%5 == 0:
-            # checks accuracy
-            test_pred = [max(range(len(i)), key=lambda j: i[j].data) for i in
-                        map(n, test_x[:100])]
-            # returns highest activated neuron in output layer ie: which digit
-            # test predictions, of first 30
-
-            # check accuracy on test data
-            suc = 0
-            for i in range(100):
-                if test_pred[i] == test_lab[i]:
-                    suc+=1
-            print("Accuracy on test data:", suc/100.0)
-
+            counter+=batchsize
+            if batch%5 == 0:
+                # checks accuracy
+                test_pred = [max(range(len(i)), key=lambda j: i[j].data) for i in
+                            map(n, test_x[:100])]
+                # returns highest activated neuron in output layer ie: which digit
+                suc = 0
+                for i in range(100):
+                    if test_pred[i] == test_lab[i]:
+                        suc+=1
+                print("Accuracy on test data:", suc, "%")
+    test_pred = [max(range(len(i)), key=lambda j: i[j].data) for i in map(n, test_x[:1000])]
+    # returns highest activated neuron in output layer ie: which digit
+    suc = 0
+    for i in range(1000):
+        if test_pred[i] == test_lab[i]:
+            suc+=1
+    print("Accuracy on test data (1000):", suc/1000.0)
     # write weights to a file
     with open('weights.pkl', 'wb') as file:
         pickle.dump([i.data for i in n.parameters()], file)
 
-train()
-# print("Label:", [i.data for i in train_y[12]])
-# pred = n(train_x[12])
-# print("Prediction:     ", [round(i.data,4) for i in pred])
-# loss, acc = findloss([pred], [train_y[12]])
-# print(loss.data, acc)
-# for p in n.parameters():
-#     p.grad = 0.0
-# loss.backwards()
-# learningrate = 1
-# for p in n.parameters():
-#     p.data -= learningrate * p.grad
-# newpred = n(train_x[12])
-# print("New Prediction1:", [round(i.data, 4) for i in newpred])
-# loss, acc = findloss([newpred], [train_y[12]])
-# print(loss.data, acc)
-# for p in n.parameters():
-#     p.grad = 0.0
-# loss.backwards()
-# for p in n.parameters():
-#     p.data -= learningrate * p.grad
-# newpred = n(train_x[12])
-# print("New Prediction2:", [round(i.data, 4) for i in newpred])
-# loss, acc = findloss([newpred], [train_y[12]])
-# print(loss.data, acc)
-# for p in n.parameters():
-#     p.grad = 0.0
-# loss.backwards()
-# for p in n.parameters():
-#     p.data -= learningrate * p.grad
-# newpred = n(train_x[12])
-# print("New Prediction3:", [round(i.data, 4) for i in newpred])
+train(100, 1, 10, 100)
+
+# b=12
+# s=8
+
+# newpred = list(map(n, train_x[b:b+s]))
+# print("Prediction:     ", *newpred, sep="\n")
+
+# while True:
+#     inp = input()
+#     if inp=="q":
+#         break
+#     if inp=="b":
+#         b+=s
+
+#     loss, acc = findloss(newpred, train_y[b:b+s])
+#     print(loss.data, acc)
+#     for p in n.parameters():
+#         p.grad = 0.0
+#     loss.backwards()
+#     learningrate = 0.1
+#     for p in n.parameters():
+#         p.data -= learningrate * p.grad
+#     newpred = list(map(n, train_x[b:b+s]))
+#     #print("New Prediction1:", *newpred, sep="\n")
